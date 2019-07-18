3e4be0eee98cc00ad83e9d8509523f8e67054679
gergokutu <gergo.kutu@gmail.com>
2019-07-18 11:45:53 +0200

    object recap

--- object-experiments.js
+++ object-experiments.js
@@ -1,12 +1,46 @@
-const objectOne = {
+// const objectOne = {
+//   firstName: 'Alice',
+//   lastName: 'the Rabbit Hunter'
+// }
+
+// console.log('Value of Object One: ', objectOne)
+
+// // not work this way » point of reference!!!
+// const objectTwo = objectOne
+// objectTwo.lastName = 'The nice girl'
+
+// console.log('Value of Object One: ', objectOne)
+// console.log('Value of Object Two: ', objectTwo)
+
+const hunter = {
   firstName: 'Alice',
   lastName: 'the Rabbit Hunter'
 }
 
-console.log('Value of Object One: ', objectOne)
+console.log('Value of Object hunter: ', hunter)
 
-const objectTwo = objectOne
-objectTwo.lastName = 'The nice girl'
+const niceGirl = { ...hunter } // shallow copy!!!
+niceGirl.lastName = 'The nice girl'
 
-console.log('Value of Object One: ', objectOne)
-console.log('Value of Object Two: ', objectTwo)
+// this now works as expected
+console.log('Value of Object hunter: ', hunter)
+console.log('Value of Object niceGirl: ', niceGirl)
+
+// now we create an object that has a nested object:
+const cast = {
+  hunter,
+  prey: 'The Rabbit'
+}
+
+console.log('Value of Object hunter: ', hunter)
+console.log('Value of Object cast: ', cast)
+
+// let's mutate our hunter
+hunter.firstName = 'bob'
+
+console.log('Value of Object hunter: ', hunter)
+console.log('Value of Object cast: ', cast)
+
+const hunterExtended = {...hunter, extension: 'first'}
+
+console.log('Value of Object hunter: ', hunterExtended)
